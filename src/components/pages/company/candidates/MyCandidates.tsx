f578bd1dd6c06d2997236007ffad39c8615bc7b0
vaibhav9045 <vaibhav.rastogi@fleekitsolutions.com>
2024-12-19 15:58:42 +0530

    small fix

--- src/components/pages/company/candidates/MyCandidates.tsx
+++ src/components/pages/company/candidates/MyCandidates.tsx
@@ -10,10 +10,11 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const applicantData = [
   {
+    id:1,
     vendor: "Fleek IT Solutions",
     name: "Harshit Tandon",
     requirement: "React js Developer",
@@ -21,9 +22,10 @@ const applicantData = [
     status: "In Review",
     date: "13-07-2024",
     vendorLogo: "https://fleekitsolutions.com/wp-content/uploads/2023/09/favicon-32x32-1.png",
-    clientLogo:"https://assets.airtel.in/static-assets/new-home/img/favicon-16x16.png",
+    clientLogo: "https://assets.airtel.in/static-assets/new-home/img/favicon-16x16.png",
   },
   {
+    id:2,
     vendor: "DevStringX Technologies",
     name: "Raj Pathar",
     requirement: "Sr. Angular developer",
@@ -34,6 +36,7 @@ const applicantData = [
     clientLogo: "https://www.ibm.com/content/dam/adobe-cms/default-images/favicon.svg",
   },
   {
+    id:3,
     vendor: "Binemiles Technologies",
     name: "Sajid Sarkar",
     requirement: "React Native mobile developer",
@@ -41,9 +44,10 @@ const applicantData = [
     status: "Declined",
     date: "18-05-2024",
     vendorLogo: "https://www.capgemini.com/wp-content/uploads/2021/06/cropped-favicon.png?w=192",
-    clientLogo:"https://www.capgemini.com/wp-content/uploads/2021/06/cropped-favicon.png?w=192",
+    clientLogo: "https://www.capgemini.com/wp-content/uploads/2021/06/cropped-favicon.png?w=192",
   },
   {
+    id:4,
     vendor: "SDET Tech Pvt. Ltd",
     name: "Amit Kumar",
     requirement: "Frontend developer",
@@ -51,9 +55,10 @@ const applicantData = [
     status: "Hired",
     date: "11-04-2024",
     vendorLogo: "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
-    clientLogo:"https://www.nttdata.com/global/en/-/media/assets/images/android-chrome-256256.png?rev=8dd26dac893a4a07bae174ff25e900ef",
+    clientLogo: "https://www.nttdata.com/global/en/-/media/assets/images/android-chrome-256256.png?rev=8dd26dac893a4a07bae174ff25e900ef",
   },
   {
+    id:5,
     vendor: "Fleek IT Solutions",
     name: "Harshit Tandon",
     requirement: ".Net developer",
@@ -61,9 +66,10 @@ const applicantData = [
     status: "Hired",
     date: "13-07-2024",
     vendorLogo: "https://fleekitsolutions.com/wp-content/uploads/2023/09/favicon-32x32-1.png",
-    clientLogo:"https://assets.airtel.in/static-assets/new-home/img/favicon-16x16.png",
+    clientLogo: "https://assets.airtel.in/static-assets/new-home/img/favicon-16x16.png",
   },
   {
+    id:6,
     vendor: "SDET Tech Pvt. Ltd",
     name: "Vaibhav Rastogi",
     requirement: "Devops AWS Certified engineer",
@@ -74,6 +80,7 @@ const applicantData = [
     clientLogo: "https://www.nttdata.com/global/en/-/media/assets/images/android-chrome-256256.png?rev=8dd26dac893a4a07bae174ff25e900ef",
   },
   {
+    id:7,
     vendor: "DevStringX Technologies",
     name: "Raj Pathar",
     requirement: ".Net MVC Support",
@@ -84,6 +91,7 @@ const applicantData = [
     clientLogo: "https://www.ibm.com/content/dam/adobe-cms/default-images/favicon.svg",
   },
   {
+    id:8,
     vendor: "Binemiles Technologies",
     name: "Sajid Sarkar",
     requirement: "Azure Devops Engineer",
@@ -91,9 +99,10 @@ const applicantData = [
     status: "Declined",
     date: "18-05-2024",
     vendorLogo: "https://binmile.com/wp-content/uploads/2022/07/bmt-favicon.png",
-    clientLogo:"https://binmile.com/wp-content/uploads/2022/07/bmt-favicon.png",
+    clientLogo: "https://binmile.com/wp-content/uploads/2022/07/bmt-favicon.png",
   },
   {
+    id:9,
     vendor: "SDET Tech Pvt. Ltd",
     name: "Amit Kumar",
     requirement: "Devops AWS Certified engineer",
@@ -101,7 +110,7 @@ const applicantData = [
     status: "Interview Scheduled ",
     date: "11-04-2024",
     vendorLogo: "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
-    clientLogo:"https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
+    clientLogo: "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
   },
 ];
 
@@ -124,132 +133,142 @@ export default function MyCandidates() {
     searchValue: "",
     status: !params?.status ? "" : params?.status,
   });
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    // Filtering logic
-    const filtered = applicantData.filter((item) => {
-      // Check status filter
-      const statusMatch =
-        searchFilter.status === "" || item.status === searchFilter.status;
-      // Check search input
-      const searchMatch =
-        searchFilter.searchValue === "" ||
-        item.name
-          .toLowerCase()
-          .includes(searchFilter.searchValue.toLowerCase());
-      return statusMatch && searchMatch;
-    });
-    setFilteredApplicants(filtered);
-  }, [searchFilter]);
+  const handleRowClick = (id: number, type: string) => {
+    switch (type) {
+      case 'vendor':
+        navigate(`/company/myvendors/${id}`);
+        break;
+      case 'client':
+        navigate(`/company/clients/${id}`);
+        break;
+    };
+  }
 
-  return (
-    <div className="px-4 py-1">
-      <Box className="flex items-center justify-end my-2">
-        <Box className="flex items-center space-x-4">
-          <TextField
-            variant="outlined"
-            size="small"
-            placeholder="Search Candidate"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            InputProps={{
-              startAdornment: <Search className="mr-2" fontSize="small" />,
-            }}
-          />
-          <FormControl size="small">
-            <InputLabel id="topic">Status</InputLabel>
-            <Select
-              className="rounded !w-[150px] bg-primary-light"
-              label="Topic"
-              value={searchFilter.client}
-              onChange={(event) => {
-                setSearchFilter((prev: any) => ({
-                  ...prev,
-                  status: event.target.value,
-                }));
+    useEffect(() => {
+      // Filtering logic
+      const filtered = applicantData.filter((item) => {
+        // Check status filter
+        const statusMatch =
+          searchFilter.status === "" || item.status === searchFilter.status;
+        // Check search input
+        const searchMatch =
+          searchFilter.searchValue === "" ||
+          item.name
+            .toLowerCase()
+            .includes(searchFilter.searchValue.toLowerCase());
+        return statusMatch && searchMatch;
+      });
+      setFilteredApplicants(filtered);
+    }, [searchFilter]);
+
+    return (
+      <div className="px-4 py-1">
+        <Box className="flex items-center justify-end my-2">
+          <Box className="flex items-center space-x-4">
+            <TextField
+              variant="outlined"
+              size="small"
+              placeholder="Search Candidate"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              InputProps={{
+                startAdornment: <Search className="mr-2" fontSize="small" />,
               }}
-            >
-              <MenuItem value={""}>All</MenuItem>
-              {filterList?.status?.map((status: any) => (
-                <MenuItem key={status} value={status}>
-                  {status}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Button variant="outlined" startIcon={<FilterList />}>
-            Filter
-          </Button>
-        </Box>
-      </Box>
-      <div className="table-body">
-        <table>
-          <thead>
-            <tr>
-              <th className="add-right-shadow">Candidate Name</th>
-              <th>Vendor</th>
-              <th>Requirement</th>
-              <th>Client</th>
-              <th>Status</th>
-              <th>Application Date</th>
-              <th>CV</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredApplicants.map((applicant, index) => (
-              <tr
-                className="cursor-pointer"
-                key={index}
-                // onClick={() => handleRowClick(applicant.id)}
+            />
+            <FormControl size="small">
+              <InputLabel id="topic">Status</InputLabel>
+              <Select
+                className="rounded !w-[150px] bg-primary-light"
+                label="Topic"
+                value={searchFilter.client}
+                onChange={(event) => {
+                  setSearchFilter((prev: any) => ({
+                    ...prev,
+                    status: event.target.value,
+                  }));
+                }}
               >
-                <th className="add-right-shadow">{applicant.name}</th>
-                <td className="wide-250">
-                  <div className="flex ">
-                    <img
-                      src={applicant.vendorLogo}
-                      style={{ height: 16, width: 16 }}
-                      className="me-1"
-                    />
-                    {applicant.vendor}
-                  </div>
-                  </td>
-                <td>{applicant.requirement}</td>
-                <td className="wide-250">
-                <div className="flex">
-                    <img
-                      src={applicant.clientLogo}
-                      style={{ height: 16, width: 16 }}
-                      className="me-1"
-                    />
-                    {applicant.client}
-                  </div>
-                </td>
-                <td>
-                  <Typography
-                    className={`inline-block px-3 py-1 !text-base rounded-full ${
-                      applicant.status === "Hired"
-                        ? "bg-green-100 text-green-700"
-                        : "bg-red-100 text-red-700"
-                    }`}
-                  >
-                    {applicant.status}
-                  </Typography>
-                </td>
-                <td>{applicant.date}</td>
-                <td>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    startIcon={<PictureAsPdf />}
-                  >
-                    Download
-                  </Button>
-                </td>
+                <MenuItem value={""}>All</MenuItem>
+                {filterList?.status?.map((status: any) => (
+                  <MenuItem key={status} value={status}>
+                    {status}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Button variant="outlined" startIcon={<FilterList />}>
+              Filter
+            </Button>
+          </Box>
+        </Box>
+        <div className="table-body">
+          <table>
+            <thead>
+              <tr>
+                <th className="add-right-shadow">Candidate Name</th>
+                <th>Vendor</th>
+                <th>Requirement</th>
+                <th>Client</th>
+                <th>Status</th>
+                <th>Application Date</th>
+                <th>CV</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredApplicants.map((applicant, index) => (
+                <tr
+                  key={index}
+                // onClick={() => handleRowClick(applicant.id)}
+                >
+                  <th className="add-right-shadow">{applicant.name}</th>
+                  <td className="wide-250 cursor-pointer" >
+                    <div className="flex" onClick={() => handleRowClick(applicant.id,'vendor')}>
+                      <img
+                        src={applicant.vendorLogo}
+                        style={{ height: 16, width: 16 }}
+                        className="me-1"
+                      />
+                      {applicant.vendor}
+                    </div>
+                  </td>
+                  <td>{applicant.requirement}</td>
+                  <td className="wide-250 cursor-pointer" onClick={() => handleRowClick(applicant.id,'client')}>
+                    <div className="flex">
+                      <img
+                        src={applicant.clientLogo}
+                        style={{ height: 16, width: 16 }}
+                        className="me-1"
+                      />
+                      {applicant.client}
+                    </div>
+                  </td>
+                  <td>
+                    <Typography
+                      className={`inline-block px-3 py-1 !text-base rounded-full ${applicant.status === "Hired"
+                          ? "bg-green-100 text-green-700"
+                          : "bg-red-100 text-red-700"
+                        }`}
+                    >
+                      {applicant.status}
+                    </Typography>
+                  </td>
+                  <td>{applicant.date}</td>
+                  <td>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      startIcon={<PictureAsPdf />}
+                    >
+                      Download
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
