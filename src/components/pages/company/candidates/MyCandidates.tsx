87927aa6937b92544dd251bcefefe6a56fca5ee4
Harshit-TandonHT <harshit.tandon@fleekitsolutions.com>
2024-12-20 17:12:15 +0530

    create new dialog and filter changes

--- src/components/pages/company/candidates/MyCandidates.tsx
+++ src/components/pages/company/candidates/MyCandidates.tsx
@@ -1,22 +1,19 @@
+import MenuDrpDwn from "../../../../components/shared/MenuDrpDwn";
+import StatusDialog from "../../../../components/shared/StatusDialog";
+import MatchingSkillsDialog from "../../../../components/shared/MatchingSkillsDialog";
+import { Download } from "@mui/icons-material";
 import {
-  Download,
-  FilterList,
-  PictureAsPdf,
-  Search,
-} from "@mui/icons-material";
-import {
-  Box,
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
+  IconButton,
+  InputAdornment,
   TextField,
   Tooltip,
   Typography,
 } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import FilterListOutlinedIcon from "@mui/icons-material/FilterListOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
 
 const applicantData = [
   {
@@ -26,6 +23,7 @@ const applicantData = [
     client: "Airtel",
     status: "In Review",
     date: "13-07-2024",
+    ai: 60,
     vendorLogo:
       "https://fleekitsolutions.com/wp-content/uploads/2023/09/favicon-32x32-1.png",
     clientLogo:
@@ -36,8 +34,9 @@ const applicantData = [
     name: "Raj Pathar",
     requirement: "Sr. Angular developer",
     client: "IBM Consulting",
-    status: "Shortlisted",
+    status: "New",
     date: "12-06-2024",
+    ai: 70,
     vendorLogo:
       "https://www.devstringx.com/wp-content/uploads/2018/03/favicon.ico",
     clientLogo:
@@ -48,8 +47,9 @@ const applicantData = [
     name: "Sajid Sarkar",
     requirement: "React Native mobile developer",
     client: "Capgemini",
-    status: "Declined",
+    status: "Rejected",
     date: "18-05-2024",
+    ai: 50,
     vendorLogo:
       "https://www.capgemini.com/wp-content/uploads/2021/06/cropped-favicon.png?w=192",
     clientLogo:
@@ -60,8 +60,9 @@ const applicantData = [
     name: "Amit Kumar",
     requirement: "Frontend developer",
     client: "NTT DATA",
-    status: "Hired",
+    status: "Placed",
     date: "11-04-2024",
+    ai: 80,
     vendorLogo:
       "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
     clientLogo:
@@ -72,8 +73,9 @@ const applicantData = [
     name: "Harshit Tandon",
     requirement: ".Net developer",
     client: "Airtel",
-    status: "Hired",
+    status: "Placed",
     date: "13-07-2024",
+    ai: 65,
     vendorLogo:
       "https://fleekitsolutions.com/wp-content/uploads/2023/09/favicon-32x32-1.png",
     clientLogo:
@@ -84,8 +86,9 @@ const applicantData = [
     name: "Vaibhav Rastogi",
     requirement: "Devops AWS Certified engineer",
     client: "NTT DATA",
-    status: "Interview Scheduled",
+    status: "Interview Round I",
     date: "11-04-2024",
+    ai: 76,
     vendorLogo:
       "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
     clientLogo:
@@ -98,6 +101,7 @@ const applicantData = [
     client: "IBM Consulting",
     status: "Shortlisted",
     date: "12-06-2024",
+    ai: 56,
     vendorLogo:
       "https://www.devstringx.com/wp-content/uploads/2018/03/favicon.ico",
     clientLogo:
@@ -108,8 +112,9 @@ const applicantData = [
     name: "Sajid Sarkar",
     requirement: "Azure Devops Engineer",
     client: "Capgemini",
-    status: "Declined",
+    status: "Rejected",
     date: "18-05-2024",
+    ai: 82,
     vendorLogo:
       "https://binmile.com/wp-content/uploads/2022/07/bmt-favicon.png",
     clientLogo:
@@ -120,8 +125,9 @@ const applicantData = [
     name: "Amit Kumar",
     requirement: "Devops AWS Certified engineer",
     client: "NTT DATA",
-    status: "Interview Scheduled ",
+    status: "Interview Round I",
     date: "11-04-2024",
+    ai: 55,
     vendorLogo:
       "https://sdettech.com/wp-content/themes/sdetech/assets/images/favicon.png",
     clientLogo:
@@ -133,21 +139,29 @@ export default function MyCandidates() {
   const navigate = useNavigate();
   const location = useLocation();
   const params = location.state || {};
-  const [search, setSearch] = useState("");
   const [filteredApplicants, setFilteredApplicants] = useState<any[]>([]);
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+  const [isMatchOpen, setIsMatchOpen] = React.useState(false);
+  const [selectedStatus, setSelectedStatus] = React.useState("New");
+  const [matchingScore, setMatchingScore] = React.useState(0);
 
   const [filterList, setFilterList] = useState<any>({
+    client: ["Airtel", "IBM Consulting", "Capgemini", "NTT DATA"],
     status: [
+      "New",
       "In Review",
       "Shortlisted",
-      "Interview Scheduled",
-      "Declined",
-      "Hired",
+      "Technical Assessment",
+      "Interview Round I",
+      "Interview Round II",
+      "Rejected",
+      "Placed",
     ],
   });
   const [searchFilter, setSearchFilter] = useState<any>({
     searchValue: "",
-    status: !params?.status ? "" : params?.status,
+    client: [],
+    status: !params?.status ? [] : [params?.status],
   });
 
   const handleRowClick = (id: number, type: string) => {
@@ -166,66 +180,95 @@ export default function MyCandidates() {
     const filtered = applicantData.filter((item) => {
       // Check status filter
       const statusMatch =
-        searchFilter.status === "" || item.status === searchFilter.status;
+        searchFilter.status.length === 0 ||
+        searchFilter.status.includes(item.status);
+      // Check client filter
+      const clientMatch =
+        searchFilter.client.length === 0 ||
+        searchFilter.client.includes(item.client);
       // Check search input
       const searchMatch =
         searchFilter.searchValue === "" ||
         item.name
           .toLowerCase()
           .includes(searchFilter.searchValue.toLowerCase());
-      return statusMatch && searchMatch;
+      return statusMatch && searchMatch && clientMatch;
     });
     setFilteredApplicants(filtered);
-  }, [searchFilter]);
+  }, [searchFilter, setFilteredApplicants]);
+
+  const handleStatusDialog = (status: string) => {
+    setIsDialogOpen(true);
+    setSelectedStatus(status);
+  };
+
+  const handleMatchingDialog = (score: number) => {
+    setIsMatchOpen(true);
+    setMatchingScore(score);
+  };
 
   return (
     <div className="px-4 py-1">
-      <Box className="flex items-center justify-end my-2">
-        <Box className="flex items-center space-x-4">
-          <TextField
-            variant="outlined"
-            size="small"
-            placeholder="Search Candidate"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            InputProps={{
-              startAdornment: <Search className="mr-2" fontSize="small" />,
-            }}
-          />
-          <FormControl size="small">
-            <InputLabel id="topic">Status</InputLabel>
-            <Select
-              className="rounded !w-[150px] bg-primary-light"
-              label="Topic"
-              value={searchFilter.client}
-              onChange={(event) => {
-                setSearchFilter((prev: any) => ({
-                  ...prev,
-                  status: event.target.value,
-                }));
-              }}
-            >
-              <MenuItem value={""}>All</MenuItem>
-              {filterList?.status?.map((status: any) => (
-                <MenuItem key={status} value={status}>
-                  {status}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Button variant="outlined" startIcon={<FilterList />}>
-            Filter
-          </Button>
-        </Box>
-      </Box>
+      <div className="flex flex-row gap-1 justify-end mb-1">
+        <div className="flex flex-row gap-1 p-1 overflow-hidden">
+          <div className="flex text-center flex-nowrap my-auto">
+            <div className="flex grow w-[220px] mr-2">
+              <div className="flex-col flex-grow">
+                <TextField
+                  size="small"
+                  className="w-full"
+                  value={searchFilter.searchValue}
+                  onChange={(event) =>
+                    setSearchFilter({
+                      ...searchFilter,
+                      searchValue: event.target.value,
+                    })
+                  }
+                  placeholder="Search"
+                  slotProps={{
+                    input: {
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SearchIcon fontSize="inherit" />
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                />
+              </div>
+            </div>
+            <div className="max-w-full shrink-0">
+              <MenuDrpDwn
+                menuList={filterList?.client}
+                placeholder="Client"
+                handleSelectedItem={(selectedItems) => {
+                  setSearchFilter({ ...searchFilter, client: selectedItems });
+                }}
+              />
+            </div>
+            <div className="max-w-full shrink-0">
+              <MenuDrpDwn
+                menuList={filterList?.status}
+                placeholder="Status"
+                handleSelectedItem={(selectedItems) => {
+                  setSearchFilter({ ...searchFilter, status: selectedItems });
+                }}
+              />
+            </div>
+          </div>
+          <IconButton aria-label="filter">
+            <FilterListOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
       <div className="table-body">
         <table>
           <thead>
             <tr>
               <th className="add-right-shadow">Candidate Name</th>
               {/* <th>Vendor</th> */}
-              <th>Requirement</th>
-              <th>Client</th>
+              <th>Role</th>
+              <th className="wide-200">Client</th>
               <th>Status</th>
               <th>Application Date</th>
               {/* <th>CV</th> */}
@@ -253,38 +296,43 @@ export default function MyCandidates() {
                       </Tooltip>
                     </div>
                     <div className="flex text-info items-center">
-                      <svg
-                        width="14px"
-                        height="14px"
-                        viewBox="0 0 512 512"
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
+                      <div
+                        className="flex cursor-pointer"
+                        onClick={() => handleMatchingDialog(applicant.ai)}
                       >
-                        <title>ai</title>
-                        <g
-                          id="Page-1"
-                          stroke="none"
-                          stroke-width="1"
-                          fill="none"
-                          fill-rule="evenodd"
+                        <svg
+                          width="14px"
+                          height="14px"
+                          viewBox="0 0 512 512"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
                         >
+                          <title>ai</title>
                           <g
-                            id="icon"
-                            fill="#4640DE"
-                            transform="translate(64.000000, 64.000000)"
+                            id="Page-1"
+                            stroke="none"
+                            stroke-width="1"
+                            fill="none"
+                            fill-rule="evenodd"
                           >
-                            <path
-                              d="M320,64 L320,320 L64,320 L64,64 L320,64 Z M171.749388,128 L146.817842,128 L99.4840387,256 L121.976629,256 L130.913039,230.977 L187.575039,230.977 L196.319607,256 L220.167172,256 L171.749388,128 Z M260.093778,128 L237.691519,128 L237.691519,256 L260.093778,256 L260.093778,128 Z M159.094727,149.47526 L181.409039,213.333 L137.135039,213.333 L159.094727,149.47526 Z M341.333333,256 L384,256 L384,298.666667 L341.333333,298.666667 L341.333333,256 Z M85.3333333,341.333333 L128,341.333333 L128,384 L85.3333333,384 L85.3333333,341.333333 Z M170.666667,341.333333 L213.333333,341.333333 L213.333333,384 L170.666667,384 L170.666667,341.333333 Z M85.3333333,0 L128,0 L128,42.6666667 L85.3333333,42.6666667 L85.3333333,0 Z M256,341.333333 L298.666667,341.333333 L298.666667,384 L256,384 L256,341.333333 Z M170.666667,0 L213.333333,0 L213.333333,42.6666667 L170.666667,42.6666667 L170.666667,0 Z M256,0 L298.666667,0 L298.666667,42.6666667 L256,42.6666667 L256,0 Z M341.333333,170.666667 L384,170.666667 L384,213.333333 L341.333333,213.333333 L341.333333,170.666667 Z M0,256 L42.6666667,256 L42.6666667,298.666667 L0,298.666667 L0,256 Z M341.333333,85.3333333 L384,85.3333333 L384,128 L341.333333,128 L341.333333,85.3333333 Z M0,170.666667 L42.6666667,170.666667 L42.6666667,213.333333 L0,213.333333 L0,170.666667 Z M0,85.3333333 L42.6666667,85.3333333 L42.6666667,128 L0,128 L0,85.3333333 Z"
-                              id="Combined-Shape"
-                            ></path>
+                            <g
+                              id="icon"
+                              fill="#4640DE"
+                              transform="translate(64.000000, 64.000000)"
+                            >
+                              <path
+                                d="M320,64 L320,320 L64,320 L64,64 L320,64 Z M171.749388,128 L146.817842,128 L99.4840387,256 L121.976629,256 L130.913039,230.977 L187.575039,230.977 L196.319607,256 L220.167172,256 L171.749388,128 Z M260.093778,128 L237.691519,128 L237.691519,256 L260.093778,256 L260.093778,128 Z M159.094727,149.47526 L181.409039,213.333 L137.135039,213.333 L159.094727,149.47526 Z M341.333333,256 L384,256 L384,298.666667 L341.333333,298.666667 L341.333333,256 Z M85.3333333,341.333333 L128,341.333333 L128,384 L85.3333333,384 L85.3333333,341.333333 Z M170.666667,341.333333 L213.333333,341.333333 L213.333333,384 L170.666667,384 L170.666667,341.333333 Z M85.3333333,0 L128,0 L128,42.6666667 L85.3333333,42.6666667 L85.3333333,0 Z M256,341.333333 L298.666667,341.333333 L298.666667,384 L256,384 L256,341.333333 Z M170.666667,0 L213.333333,0 L213.333333,42.6666667 L170.666667,42.6666667 L170.666667,0 Z M256,0 L298.666667,0 L298.666667,42.6666667 L256,42.6666667 L256,0 Z M341.333333,170.666667 L384,170.666667 L384,213.333333 L341.333333,213.333333 L341.333333,170.666667 Z M0,256 L42.6666667,256 L42.6666667,298.666667 L0,298.666667 L0,256 Z M341.333333,85.3333333 L384,85.3333333 L384,128 L341.333333,128 L341.333333,85.3333333 Z M0,170.666667 L42.6666667,170.666667 L42.6666667,213.333333 L0,213.333333 L0,170.666667 Z M0,85.3333333 L42.6666667,85.3333333 L42.6666667,128 L0,128 L0,85.3333333 Z"
+                                id="Combined-Shape"
+                              ></path>
+                            </g>
                           </g>
-                        </g>
-                      </svg>
-                      <span>80%</span>
-                      <Button variant="text" size="small">
+                        </svg>
+                        <span> {applicant.ai}%</span>
+                      </div>
+                      <div className="ms-2 text-indigo-500 cursor-pointer hover:text-indigo-700 ">
                         <Download fontSize="inherit" />
                         <span className="text-info">CV</span>
-                      </Button>
+                      </div>
                     </div>
                   </div>
                 </th>
@@ -300,7 +348,7 @@ export default function MyCandidates() {
                 </td> */}
                 <td>{applicant.requirement}</td>
                 <td
-                  className="wide-250 cursor-pointer hover:text-indigo-700"
+                  className="wide-200 cursor-pointer hover:text-indigo-700"
                   onClick={() => handleRowClick(applicant.id, "client")}
                 >
                   <div className="flex">
@@ -314,11 +362,16 @@ export default function MyCandidates() {
                 </td>
                 <td>
                   <Typography
-                    className={`inline-block px-3 py-1 !text-base rounded-full ${
-                      applicant.status === "Hired"
+                    className={`inline-block px-3 py-1 !text-base rounded-full cursor-pointer ${
+                      applicant.status === "Placed"
                         ? "bg-green-100 text-green-700"
-                        : "bg-red-100 text-red-700"
+                        : applicant.status === "Rejected"
+                        ? "bg-red-100 text-red-700"
+                        : applicant.status === "New"
+                        ? "bg-orange-100 text-orange-700"
+                        : "bg-indigo-100 text-indigo-700"
                     }`}
+                    onClick={() => handleStatusDialog(applicant.status)}
                   >
                     {applicant.status}
                   </Typography>
@@ -338,6 +391,20 @@ export default function MyCandidates() {
           </tbody>
         </table>
       </div>
+
+      <StatusDialog
+        title="Applicant Status"
+        statusData={filterList.status}
+        isDialogOpen={isDialogOpen}
+        setIsDialogOpen={setIsDialogOpen}
+        selectedStatus={selectedStatus}
+      />
+      <MatchingSkillsDialog
+        title="Matching Score Analysis"
+        isMatchOpen={isMatchOpen}
+        setIsMatchOpen={setIsMatchOpen}
+        aiScore={matchingScore}
+      />
     </div>
   );
 }
---
+++ src/components/shared/MatchingSkillsDialog.tsx
@@ -0,0 +1,113 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { CircularProgressWithLabel } from "./CircularProgressWithLabel";
+import {
+  CancelOutlined,
+  CheckBox,
+  CheckBoxOutlined,
+  CheckCircleOutlineOutlined,
+} from "@mui/icons-material";
+
+export default function MatchingSkillsDialog({
+  title = "",
+  isMatchOpen = false,
+  setIsMatchOpen = (value: boolean) => {},
+  aiScore = 0,
+}: any) {
+  const handleClose = () => {
+    setIsMatchOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={isMatchOpen}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {/* <DialogTitle id="alert-dialog-title" className="!text-heading">
+          {title}
+        </DialogTitle> */}
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <div className="min-w-[400px] max-w-[600px]">
+              <CircularProgressWithLabel value={aiScore} />
+
+              <h5 className="text-title my-2 font-bold">SKill Analysis</h5>
+
+              <div className="flex items-center text-title mb-1">
+                <CheckCircleOutlineOutlined
+                  className="text-green-500"
+                  fontSize="small"
+                />
+                <p className="text-base ms-1">
+                  Java, React, Python, REST APIs.
+                </p>
+              </div>
+
+              <div className="flex items-center text-title mb-1">
+                <CancelOutlined className="text-red-500" fontSize="small" />
+                <p className="text-base ms-1">AWS, Docker, Kubernetes.</p>
+              </div>
+
+              <h5 className="text-title my-2 font-bold">Experience</h5>
+              <div className="flex items-center text-title mb-1">
+                <CancelOutlined className="text-orange-500" fontSize="small" />
+                <p className="text-base ms-1">
+                  Job requires 5+ years; candidate has 4 years (slight gap).
+                </p>
+              </div>
+
+              <h5 className="text-title my-2 font-bold">Education</h5>
+
+              <div className="flex items-center text-title mb-1">
+                <CheckCircleOutlineOutlined
+                  className="text-green-500"
+                  fontSize="small"
+                />
+                <p className="text-base ms-1">
+                  Job requires a Bachelor's in CS; candidate has a Bachelor's in
+                  IT (closely aligned).
+                </p>
+              </div>
+
+              <h5 className="text-title my-2 font-bold">Summary</h5>
+
+              <div className="flex items-center text-title mb-1">
+                <CheckCircleOutlineOutlined
+                  className="text-green-500"
+                  fontSize="small"
+                />
+                <p className="text-base ms-1">
+                  Strong in programming languages and APIs.
+                </p>
+              </div>
+
+              <div className="flex items-center text-title mb-1">
+                <CancelOutlined className="text-orange-500" fontSize="small" />
+                <p className="text-base ms-1">
+                  Missing cloud-related skills like AWS and containerization
+                  experience.
+                </p>
+              </div>
+            </div>
+          </DialogContentText>
+          <DialogActions className="!mt-4">
+            <Button onClick={handleClose} variant="outlined" autoFocus>
+              Close
+            </Button>
+            {/* <Button onClick={handleClose} variant="contained" autoFocus>
+              Update
+            </Button> */}
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
