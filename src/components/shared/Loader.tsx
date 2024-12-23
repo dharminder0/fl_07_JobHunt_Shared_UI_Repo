ce43e0f43d52ca5592fe267e92f64adee01ff2a2
Harshit-TandonHT <harshit.tandon@fleekitsolutions.com>
2024-12-23 16:11:19 +0530

    requirment form changes

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,12 @@
+import { CircularProgress } from "@mui/material";
+import React from "react";
+
+export default function Loader({ subText1 = "", subtext2 = "" }) {
+  return (
+    <div className="flex flex-col items-center items-center justify-center h-[75vh]">
+      <CircularProgress />
+      <p className="text-base mt-4">{subText1}</p>
+      <p className="text-base mt-2">{subtext2}</p>
+    </div>
+  );
+}
